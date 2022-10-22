0bde3fd06eb06c2689d15ca31cda053864d967ac
Cam <cam@mackay.net.nz>
2022-10-22 16:06:40 +1300

    image optimization

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,23 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(`./src${src}`, {
+        widths: [300, 800, null], // the sizes (in px) of the generated images (null means original size)
+        formats: ["avif", "jpeg"], // the formats to generate the images in
+        urlPath: "/images/",
+        outputDir: "./public/images" // the location of the optimised images
+    });
+
+    let imageAttribues = {
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async"
+    };
+
+    return Image.generateHTML(metadata, imageAttribues);
+}
+
 module.exports = function(eleventyConfg) {
     // eleventyConfig is the default configuration object that Eleventy provides to customize the build process
     eleventyConfg.addWatchTarget("./src/css/"); // eleventy dev server to watch css (hot reload)
@@ -6,6 +26,10 @@ module.exports = function(eleventyConfg) {
     eleventyConfg.addPassthroughCopy({"./src/favicons": "/"}); // pass through files to the root of the build output
     
     eleventyConfg.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // image generation process is asynchronous and will take some time the different sizes and formats
+    // shortcode has to wait until these have all been generated before injecting HTML into the templates
+    eleventyConfg.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
 
     return {
         dir: {
